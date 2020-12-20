a500ab4cf42951107d767c0997313f1033da3db3
Markus Gersdorf <markus.gersdorf@googlemail.com>
2020-12-20 21:57:26 +0100

    Add Java doc comments in Agent.java.

--- ludo/src/main/de/uol/is/ludo/agents/Agent.java
+++ ludo/src/main/de/uol/is/ludo/agents/Agent.java
@@ -8,6 +8,12 @@ import sim.engine.Steppable;
 
 import java.util.ArrayList;
 
+/**
+ * Every agent or strategy must inherit from this class.
+ * n this class the basic logic is implemented, which are necessary for the game Mensch ärgere dich nicht.
+ * It is about the fact that with a matching number of dice may be rolled again.
+ * Which figure is drawn thereby decides each agent for itself.
+ */
 public abstract class Agent implements Steppable {
 
     protected String name;
@@ -15,18 +21,34 @@ public abstract class Agent implements Steppable {
     protected String strategy;
     protected IBoard board;
 
+    /**
+     * @param name  agent name
+     * @param color agent color
+     * @param board playing field
+     */
     public Agent(String name, IPawn.player color, IBoard board) {
         this.name = name;
         this.color = color;
         this.board = board;
     }
 
+    /**
+     * This method is called every time it is the agent's turn.
+     * All the logic of the agent is contained here.
+     * <p>
+     * A distinction is made between two states.
+     * If all 4 figures are in the start area, then the player may roll the dice 3 times. Otherwise, he may only roll once, unless a 6 is rolled.
+     *
+     * @param simState Simulation status
+     */
     @Override
     public void step(SimState simState) {
+        int num;
 
         if (get_remaining_number_of_pawn() == 4) {
-            int num;
-
+            // TODO: Was ist, wenn der Spieler eine 6 würfelt. Dann darf er doch noch einmal würfeln?
+            //  Evtl. den else Fall immer abarbeiten?
+            //  In dem ersten Bereich nur zweimal würfeln lassen, dann in den elese bereich gehen.
             for (int i = 0; i < 3; i++) {
                 num = board.roll();
                 if (num == 6) {
@@ -37,14 +59,13 @@ public abstract class Agent implements Steppable {
                 }
             }
         } else {
-            int num;
             do {
                 num = board.roll();
                 if (num == 6) {
                     ArrayList<IPawn> pawns = board.get_my_pawns(color);
                     for (IPawn pawn : pawns) {
                         if (pawn.get_field().get_field_type() == IField.field_type.ENTRY) {
-                            if(!board.set_pawn_into_game(pawn)) {
+                            if (!board.set_pawn_into_game(pawn)) {
                                 ArrayList<IPawn> optionList = chooseFigure();
                                 for (IPawn option_pawn : optionList) {
                                     if (board.move_pawn(option_pawn, num)) {
@@ -66,6 +87,11 @@ public abstract class Agent implements Steppable {
         }
     }
 
+    /**
+     * Count the remaining figures in the starting area
+     *
+     * @return number of figures
+     */
     protected int get_remaining_number_of_pawn() {
         ArrayList<IPawn> pawns = board.get_my_pawns(color);
         int counter_entries = 0;
@@ -77,6 +103,11 @@ public abstract class Agent implements Steppable {
         return counter_entries;
     }
 
+    /**
+     * Select a figure from the start area
+     *
+     * @return null if there is no figure in start area
+     */
     protected IPawn get_pawn_from_house() {
         ArrayList<IPawn> pawns = board.get_my_pawns(color);
         for (IPawn pawn : pawns) {
@@ -87,9 +118,14 @@ public abstract class Agent implements Steppable {
         return null;
     }
 
+    /**
+     * A list in which the game pieces are returned sorted.
+     * The figures are returned sorted by priority.
+     * The figure with the highest priority is tried to move first.
+     * If this is not possible, the next one is used.
+     *
+     * @return a list in which the game pieces are returned sorted
+     */
     protected abstract ArrayList<IPawn> chooseFigure();
 
-    public IPawn.player getPlayerColor() {
-        return color;
-    }
 }
