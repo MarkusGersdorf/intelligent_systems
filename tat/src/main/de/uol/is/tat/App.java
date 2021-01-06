33c122188f71c5aa25fb5fc80e6c3037968944ae
gett9785 <thomas.cwil@uni-oldenburg.de>
2021-01-06 15:05:05 +0100

    Added .csv-files and modified path

--- tat/src/main/de/uol/is/tat/App.java
+++ tat/src/main/de/uol/is/tat/App.java
@@ -1,5 +1,8 @@
 package de.uol.is.tat;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -30,9 +33,12 @@ public class App {
     private static ArrayList<IField[][]> fields = new ArrayList<>();
     private static IConstraints cons;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String project_path = new File("").getAbsolutePath();
+        String csv_path = project_path.concat("/tat/csv/");
+
         for(int i = 0; i < 10; i++) {
-            path[i] = "C:/Users/thoma/OneDrive/Desktop/TaT/puzzles/tents_trees_" + i + ".csv";
+            path[i] = csv_path.concat("tents_trees_" + i + ".csv");
             fields.add(reader.convert_csv_to_fields(path[i]));
         }
         fields_to_console(fields);
