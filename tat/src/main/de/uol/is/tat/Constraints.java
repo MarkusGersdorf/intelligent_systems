9e0d5af3978ef42f5bc468658131e74b1346afad
Joosten <joosten.steenhusen@uni-oldenburg.de>
2021-01-12 21:48:02 +0100

    Comments to the functions and some sorting.

--- tat/src/main/de/uol/is/tat/Constraints.java
+++ tat/src/main/de/uol/is/tat/Constraints.java
@@ -1,6 +1,5 @@
 package de.uol.is.tat;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Constraints implements IConstraints {
@@ -8,33 +7,63 @@ public class Constraints implements IConstraints {
     public Constraints(ArrayList<IField[][]> f) {
         this.fields = f;
     }
+
+    /**
+     *
+     * @param f small field (9x9)
+     * @return true if one tent stands by a tree.
+     */
     @Override
     public boolean one_tent_per_tree(IField f) {
         return false;
     }
 
+    /**
+     *
+     * @param f small field (9x9)
+     * @return true if one tree is attached to a tent.
+     */
     @Override
     public boolean one_tree_per_tent(IField f) {
         return false;
     }
 
-    @Override
-    public boolean horizontal_vertical_tent_at_tree(IField f) {
-        return false;
-    }
-
-    @Override
-    public boolean adjacent_tent_tree(IField f) {
-        return false;
-    }
-
+    /**
+     *
+     * @param f gives the hole row
+     * @param n number of tents
+     * @return true if the number of tents in the row matches with n.
+     */
     @Override
     public boolean only_n_tents_per_row(IField f, int n) {
         return false;
     }
 
+    /**
+     *
+     * @param f small field (9x9)
+     * @return true if there is no other tent in the 9x9 Matrix.
+     */
     @Override
     public boolean no_tent_around_tent(IField f) {
+        return false;
+    }
+
+    /**
+     *
+     * @deprecated
+     */
+    @Override
+    public boolean horizontal_vertical_tent_at_tree(IField f) {
+        return false;
+    }
+
+    /**
+     *
+     * @deprecated
+     */
+    @Override
+    public boolean adjacent_tent_tree(IField f) {
         return false;
     }
 }
