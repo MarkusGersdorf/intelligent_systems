30ecb891df859d6c305875dba6d7fd58a7dbfac2
Markus Gersdorf <markus.gersdorf@googlemail.com>
2021-01-20 13:43:31 +0100

    Rename module

--- shopScheduling/src/main/de/uol/is/shopSheduling/ShopScheduling.java
+++ shopScheduling/src/main/de/uol/is/shopSheduling/ShopScheduling.java
@@ -1,6 +1,6 @@
 package uol.is.shopSheduling;
 
-public class ShopSheduling {
+public class ShopScheduling {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
