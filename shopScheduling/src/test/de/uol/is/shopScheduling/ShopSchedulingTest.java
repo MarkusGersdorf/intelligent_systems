2bfb357152d4591c4c08b0f12ac029cdb4ce572a
Markus Gersdorf <markus.gersdorf@googlemail.com>
2021-01-22 20:59:10 +0100

    Add test class to assignment 3

---
+++ shopScheduling/src/test/de/uol/is/shopScheduling/ShopSchedulingTest.java
@@ -0,0 +1,17 @@
+package de.uol.is.shopScheduling;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ShopSchedulingTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
