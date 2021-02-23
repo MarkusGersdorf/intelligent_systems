96a3d4b3b2be364de0e5cfb80303730115ee61ef
Markus Gersdorf <markus.gersdorf@googlemail.com>
2021-02-23 21:55:35 +0100

    Add JavaDoc

--- shopScheduling/src/main/de/uol/is/shopScheduling/ShopScheduling.java
+++ shopScheduling/src/main/de/uol/is/shopScheduling/ShopScheduling.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.Objects;
 
 public class ShopScheduling {
     public static void main(String[] args) {
@@ -32,27 +33,23 @@ public class ShopScheduling {
 
         // now for each file generate java object
         try {
-            /*
             for (int i = 0; i < Objects.requireNonNull(listOfFiles).length; i++) {
-                FifoStrategy fifoStrategy = new FifoStrategy(jsonParser.parseJsonJobs(listOfFiles[i]), jsonParser.parseJsonResources(listOfFiles[i]));
+                //for (int i = 0; i < 1; i++) {
+                System.out.println("Path: " + listOfFiles[0].getPath().substring(55));
+                FifoStrategy fifoStrategy = new FifoStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
+                //fifoStrategy.printDiagram();
+                System.out.println("Fifo-Strategy: " + fifoStrategy.getMakespan());
+                RandomStrategy randomStrategy = new RandomStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
+                //randomStrategy.printDiagram();
+                System.out.println("Random-Strategy: " + randomStrategy.getMakespan());
+                SptStrategy sptStrategy = new SptStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
+                //sptStrategy.printDiagram();
+                System.out.println("Spt-Strategy: " + sptStrategy.getMakespan());
+                System.out.println("---------ES-Start---------");
+                EvolutionStrategy evolutionStrategy = new EvolutionStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
+                System.out.println("ES-Strategy: " + evolutionStrategy.getMakespan());
+                System.out.println("-------------------------------------------------------------------------------");
             }
-            */
-            FifoStrategy fifoStrategy = new FifoStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
-            fifoStrategy.printToConsole();
-            System.out.println("----------------------------------------------------------------");
-            System.out.println("----------------------------------------------------------------");
-            System.out.println("Fifo-Strategy: " + fifoStrategy.getMakespan());
-            RandomStrategy randomStrategy = new RandomStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
-            //randomStrategy.printDiagram();
-            System.out.println("----------------------------------------------------------------");
-            System.out.println("----------------------------------------------------------------");
-            System.out.println("Random-Strategy: " + randomStrategy.getMakespan());
-            SptStrategy sptStrategy = new SptStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
-            System.out.println("Spt-Strategy: " + sptStrategy.getMakespan());
-            System.out.println("---------ES-Start---------");
-            EvolutionStrategy evolutionStrategy = new EvolutionStrategy(jsonParser.parseJsonJobs(listOfFiles[0]), jsonParser.parseJsonResources(listOfFiles[0]));
-            System.out.println("ES-Strategy: " + evolutionStrategy.get_best_makespan());
-            evolutionStrategy.printToConsole();
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
