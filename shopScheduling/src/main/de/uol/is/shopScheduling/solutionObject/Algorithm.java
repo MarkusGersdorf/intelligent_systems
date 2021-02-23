0891710659155de0dce4afd7a06d0f227baee4b0
Markus Gersdorf <markus.gersdorf@googlemail.com>
2021-02-23 21:29:07 +0100

    Remove unnecessary methods

--- shopScheduling/src/main/de/uol/is/shopScheduling/solutionObject/Algorithm.java
+++ shopScheduling/src/main/de/uol/is/shopScheduling/solutionObject/Algorithm.java
@@ -1,7 +1,6 @@
 package de.uol.is.shopScheduling.solutionObject;
 
 import de.uol.is.shopScheduling.Job;
-import de.uol.is.shopScheduling.Resource;
 
 import java.util.ArrayList;
 
@@ -23,13 +22,4 @@ public abstract class Algorithm extends SolutionObject {
      */
     protected abstract void optimize(ArrayList<Long> resourcesArrayList);
 
-    /**
-     * initialize the population
-     *
-     * @param jobArrayList all jobs
-     * @return init population
-     */
-    protected ArrayList<Resource> initialize(ArrayList<Job> jobArrayList, ArrayList<Resource> resourceArrayList) {
-        return null;
-    }
 }
